92383d87254109fa703688f0b6a5e420e3680587
kreyuk <126581772+kreyuk@users.noreply.github.com>
2023-03-20 22:29:15 +0700

    Update kalkulasi.js

--- plugins/kalkulasi.js
+++ plugins/kalkulasi.js
@@ -1,11 +1,15 @@
-const { removeEmojis, bytesToSize, getBuffer, fetchJson, getRandom, getGroupAdmins, runtime, sleep, makeid, isUrl, generateProfilePicture } = require("../lib/func_Server")
+
+const { removeEmojis, bytesToSize, getBuffer, fetchJson, getRandom, getGroupAdmins, runtime, sleep, makeid, isUrl, generateProfilePicture } = require("../lib/func_Server");
 let handler = async (m, { args, usedPrefix, text, command }) => {
-if (!text) return m.reply(`Example: .${command} level`)
+
+if (!text) return m.reply(` .kalkulasi level
+Example: .kalkulasi 456`)
 let news = await fetchJson(`https://api.zahwazein.xyz/convert/ssweb?url=https://rucoykingdom.com/calculator/${text}&query=desktop&apikey=787520e1bc`)
 let res = news.result.url
-conn.sendMessage(m.chat, {image : {url: res}, caption: 'infonya bwang!'}, {quoted: m})
-
+conn.sendMessage(m.chat, {image : {url: res}, caption: 'Item yang menarik!'}, {quoted: m})
 }
+handler.command = /^item$/i
+handler.limit = true
 handler.command = /^(kalkulasi)$/i
 handler.limit = true
 
